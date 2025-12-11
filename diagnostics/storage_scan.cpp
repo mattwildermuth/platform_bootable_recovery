ea9e3e107b3dc76150aa1cf2a0273be75d18cf02
Matt Wildermuth <mattwildermuth@proton.me>
2025-12-10 19:23:26 -0500

    Handle a segfault generated by do_scan_storage -- hangs when scanning the userdata device

--- diagnostics/storage_scan.cpp
+++ diagnostics/storage_scan.cpp
@@ -434,20 +434,20 @@ void scan_storage(RecoveryUI* ui) {
   if (sig_hndlr_ini(SIGSEGV, segv_recover, &oldsa))
     ui->Print("AHHHHH");
 
-  int* zed = 0;
-  if (!sigsetjmp(sigenv, 0))
-    (*zed) = 6;
-  else
-    ui->Print("other side of the jmp\n");
-
-  ui->Print("err_flag: %d\n", err_flag);
+  // int* zed = 0;
+  // if (!sigsetjmp(sigenv, 0))
+  //   (*zed) = 6;
+  // else
+  //   ui->Print("other side of the jmp\n");
 
   // do_scan_storage(ui, read_dst);
 
-  // if (!sigsetjmp(sigenv, 0))
-  //   do_scan_storage(ui, read_dst);
+  if (!sigsetjmp(sigenv, 0))
+    do_scan_storage(ui, read_dst); /* hangs on userdata scan */
   // else
   //   ui->Print("FATAL\n");
+
+  ui->Print("err_flag: %d\n", err_flag);
 
   sigaction(SIGSEGV, &oldsa, 0);
   sigprocmask(SIG_SETMASK, &prev_set, 0);
